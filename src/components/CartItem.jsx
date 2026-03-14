92f5b70f8a10a0c1e6987304d1f326fe27fc1156
laxman8260 <86157348+laxman8260@users.noreply.github.com>
2026-03-15 00:30:31 +0530

    Refactor CartItem component for cleaner structure

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,41 +1,29 @@
 import React from "react";
 
-function CartItem(){
+function CartItem() {
+  return (
+    <div>
+      <h2>Shopping Cart</h2>
 
-return(
+      <div>
+        <h3>Snake Plant</h3>
 
-<div style={{padding:"40px"}}>
+        <p>Unit Price: $15</p>
 
-<h2>Shopping Cart</h2>
+        <button>+</button>
 
-<div style={{border:"1px solid gray",padding:"20px",margin:"20px"}}>
+        <button>-</button>
 
-<img src="https://i.imgur.com/1.jpg" width="120"/>
+        <button>Delete</button>
+      </div>
 
-<h3>Snake Plant</h3>
+      <h3>Total Cart Amount: $15</h3>
 
-<p>Unit Price: $15</p>
+      <button>Checkout</button>
 
-<p>Total: $15</p>
-
-<button>+</button>
-
-<button>-</button>
-
-<button>Delete</button>
-
-</div>
-
-<h3>Total Cart Amount: $15</h3>
-
-<button>Checkout</button>
-
-<button>Continue Shopping</button>
-
-</div>
-
-)
-
+      <button>Continue Shopping</button>
+    </div>
+  );
 }
 
-export default CartItem
+export default CartItem;
